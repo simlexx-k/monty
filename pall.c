c202184239694eea948d412710b1d3f201814651
Simon Kosgei <59066277+simlexx-k@users.noreply.github.com>
2024-03-29 01:02:36 +0300

    Update pall.c

--- pall.c
+++ pall.c
@@ -5,17 +5,17 @@
  * @stack: Pointer to the top of the stack.
  * @line_number: Line number being executed.
  */
-void pall(stack_t **stack, unsigned int line_number)
+void f_pall(stack_t **head, unsigned int counter)
 {
-    stack_t *current = *stack;
+	stack_t *h;
+	(void)counter;
 
-    (void)line_number;
-
-    /* Print stack values */
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
-
