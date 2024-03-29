a32ea98f56e429340cf5820af783f655bed54c04
Simon Kosgei <59066277+simlexx-k@users.noreply.github.com>
2024-03-29 09:58:32 +0300

    Update addnode.c

--- addnode.c
+++ addnode.c
@@ -1,4 +1,3 @@
-
 #include "monty.h"
 
 /**
@@ -9,21 +8,20 @@
  */
 void addnode(stack_t **head, int n)
 {
-	stack_t *new_node = malloc(sizeof(stack_t)));
+    stack_t *new_node = malloc(sizeof(stack_t));
 
-	if (!new_node)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = *head;
 
-	if (*head)
-	{
-		(*head)->prev = new_node;
-	}
-	*head = new_node;
+    if (*head)
+    {
+        (*head)->prev = new_node;
+    }
+    *head = new_node;
 }
-
